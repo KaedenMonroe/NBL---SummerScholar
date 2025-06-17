de7e6bec7120a8d2fc5e8ab92ae0bfb7fe23b11b
Kaeden Monroe <monroekaeden820@gmail.com>
2025-06-17 11:30:56 -0400

    Replace walkTrial1 and detrend plot
    walkTrial1 made more modular by creating a function detrendplot that takes the data (assigned in a structure) and plots the data in a way that is easily comparable.

--- Code/detrendplot.m
+++ Code/detrendplot.m
@@ -1,28 +1,29 @@
-detrendplot detrendplot(dataStruct)
+function detrendplot(dataStruct)
 %Takes struct, detrends yaxis, and returns a plot
 %Inputs
 %   dataStruct - Struct containing:
 %       xaxis       : x-axis values
+%       xaxisTitle  :label for x-axis in legend
 %       y1, y2      : y-axis datasets to detrend
-%       y1Title     : label for y1 in legend
-%       y2Title     : label for y2 in legend
+%       y1Title     : label for y1
+%       y2Title     : label for y2
 % Outputs
 %   combined plots
 
 % Detrending the input data
-trend_y1 = detrend(dataStruct.yaxis1);
-trend_y2 = detrend(dataStruct.yaxis2);
+trend_y1 = detrend(dataStruct.y1);
+trend_y2 = detrend(dataStruct.y2);
 
 % Creating the plot
 figure();
 hold on;
-plot(dataStruct.xaxis, trend_y1, 'r', 'DisplayName', dataStruct.yaxis1Title);
-plot(dataStruct.xaxis, trend_y2, 'b', 'DisplayName', dataStruct.yaxis2Title);
+plot(dataStruct.xaxis, trend_y1, 'r', 'DisplayName', dataStruct.y1Title);
+plot(dataStruct.xaxis, trend_y2, 'b', 'DisplayName', dataStruct.y2Title);
 
 %Add labels
-xlabel('time');
+xlabel('xaxisTitle');
 ylabel('Frequency');
 title('Detrended Signals');
 legend();
 hold off;
-end
+end 
