29674779000d7c750d8051a70204219cf4cad0cd
Kaeden Monroe <monroekaeden820@gmail.com>
2025-06-18 16:36:47 -0400

    Update and rename masked_plot.m to Active/masked_plot.m

--- Active/masked_plot.m
+++ Active/masked_plot.m
@@ -1,23 +1,25 @@
 walk1 = readtable('C:\Users\kaeden\Desktop\Code\Summer Scholar\Data\AGD16_stair.CSV');
 walk1.Properties.VariableNames = {'time', 'hindfoot', 'forefoot', 'a', 'b'};
 
+% Assigning column data to variables
 time = walk1{:,1};
 hindfoot = walk1{:,2};
 forefoot = walk1{:,3};
+
+% Defining the *arbitrary* cutoff line
 cutoff = 1.515 * 10^4;
 arbLine = [cutoff, cutoff];
+
+% logical vector above cutoff
 mask_hindfoot = hindfoot >= cutoff;
 mask_forefoot = forefoot >= cutoff;
 
 
-% Plotting baseline data
+% Plotting 'masked' data
 figure();
 hold on;
 
-% Use later : 
-% plot(time, hindfoot, 'r');                                               Original line 1
-% plot(time, forefoot, 'b');                                               Original line 2
 plot([time(1), time(end)], arbLine, 'r--', 'LineWidth', 2);   % Cutoff line
-plot(time(mask_forefoot), forefoot(mask_forefoot), 'b');
+plot(time(mask_forefoot), forefoot(mask_forefoot), 'b');      % Datapoints above Cutoff line
 
 hold off;
