904c50183d0ef57f2e00ff0e53ec001510043b1a
Kaeden Monroe <monroekaeden820@gmail.com>
2025-06-18 16:32:01 -0400

    Update and rename gen_rawplot.m to Active/gen_rawplot.m

--- Active/gen_rawplot.m
+++ Active/gen_rawplot.m
@@ -1,6 +1,7 @@
 walk1 = readtable('C:\Users\kaeden\Desktop\Code\Summer Scholar\Data\AGD16_stair.CSV');
 walk1.Properties.VariableNames = {'time', 'hindfoot', 'forefoot', 'a', 'b'}
 
+% Assigning column data to variables
 time = walk1{:,1};
 hindfoot = walk1{:,2};
 forefoot = walk1{:,3};
@@ -11,7 +12,7 @@ forefoot = walk1{:,3};
 figure();
 hold on;
 
-plot(time, hindfoot, 'red');
-plot(time, forefoot, 'blue');
+plot(time, hindfoot, 'r');
+plot(time, forefoot, 'b');
 
 hold off;
